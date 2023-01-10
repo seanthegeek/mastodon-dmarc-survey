a7bdb8aea2ec060fd4fc650c3f12481dd3f18b4d
Sean Whalen <44679+seanthegeek@users.noreply.github.com>
2023-01-10 18:05:05 -0500

    1.1.1
    - Fix typo in the `User-Agent` header
    - Swap the order of the CSV `error` and `warning` fields
    - Fix bug where `spf_valid` was always false
    - Increase DNS timeout from `0.5` seconds to `1.0` seconds
    - Add default values to the CLI help

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # Changelog
 
+##
+
+- Fix typo in the `User-Agent` header
+- Swap the order of the CSV `error` and `warning` fields
+- Fix bug where `spf_valid` was always false
+- Increase DNS timeout from `0.5` seconds to `1.0` seconds
+- Add default values to the CLI help
+
 ## 1.1.0
 
 - Rename the `active_users` field name provided by instances.social to the more accurate mame `logins_per_week` (see https://github.com/TheKinrar/instances/issues/143)
--- mastodon-dmarc-survey.py
+++ mastodon-dmarc-survey.py
@@ -2,7 +2,8 @@
 
 """Gets the DMARC policy of domains hosting Mastodon instances"""
 
-from argparse import ArgumentParser
+import logging
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from io import StringIO
 import csv
 
@@ -15,12 +16,12 @@ __version__ = "1.1.0"
 instance_csv_fields = ["name", "description", "email", "admin",
                        "logins_per_week", "users", "posts",
                        "connections", "dnssec", "spf_valid", "dmarc_policy",
-                       "warnings", "errors"]
+                       "errors", "warnings"]
 
 
 class MastodonInstancesClient:
     def __init__(self, api_key):
-        headers = {"User-Agent": user_agent("cmastodon-dmarc-surveyn", __version__),
+        headers = {"User-Agent": user_agent("mastodon-dmarc-survey", __version__),
                    "Authorization": "Bearer {}".format(api_key)}
         base_url = "https://instances.social/api/1.0/"
         self._session = sessions.BaseUrlSession(base_url=base_url)
@@ -48,7 +49,8 @@ class MastodonInstancesClient:
 def _main():
     with open("key.txt") as key_file:
         api_key = key_file.readline().strip()
-        args = ArgumentParser(description=__doc__)
+        args = ArgumentParser(description=__doc__,
+                              formatter_class=ArgumentDefaultsHelpFormatter)
         args.add_argument("--version", "-V",
                           action="version",
                           version=__version__)
@@ -59,9 +61,17 @@ def _main():
         args.add_argument("--instance", "-i", help="Get information about a single specific instance")
         args.add_argument("--json", "-j", action="store_true", help="Output in verbose JSON format")
         args.add_argument("--output", "-o", help="Redirect output to a file")
+        args.add_argument("--debug", "-d", action="store_true", help="Enable debug output")
         args = args.parse_args()
+        log_level = logging.INFO
+        if args.debug:
+            log_level = logging.DEBUG
+            logging.basicConfig(format='%(levelname)s: %(message)s',
+                                level=log_level)
+            logging.debug("Debug logging enabled...")
         client = MastodonInstancesClient(api_key)
         if args.instance is None:
+            logging.debug("Getting the list of instances...")
             instances = client.list_instances(count=args.count, include_down="false",
                                               sort_by="active_users",
                                               sort_order="desc")
@@ -71,6 +81,7 @@ def _main():
             errors = []
             warnings = []
             instance = instances[i]
+            logging.debug("Checking {}...".format(instance["name"]))
             # Convert fields names to something more accurate
             instance["posts"] = instance["statuses"]
             del instance["statuses"]
@@ -83,7 +94,7 @@ def _main():
             instance["dnssec"] = dnssec
             try:
                 dmarc_record = checkdmarc.get_dmarc_record(
-                    instance["name"], timeout=.5)
+                    instance["name"], timeout=1.0)
                 for warning in dmarc_record["parsed"]["warnings"]:
                     warnings.append("DMARC: {}".format(warning))
                 dmarc_policy = dmarc_record["parsed"]["tags"]["p"]["value"]
@@ -95,18 +106,21 @@ def _main():
                     dmarc_policy = "Enforced (p=reject)"
             except checkdmarc.DMARCRecordNotFound as e:
                 dmarc_policy = "Missing"
+                errors.append("DMARC: {}".format(str(e)))
             except checkdmarc.DMARCError as e:
                 dmarc_policy = "Invalid"
                 errors.append("DMARC: {}".format(str(e)))
             except checkdmarc.DNSException as e:
                 dmarc_policy = "Invalid"
                 errors.append("DMARC: {}".format(str(e)))
-            instance["spf_valid"] = False
+            instance["spf_valid"] = True
             try:
                 spf_record = checkdmarc.get_spf_record(
                     instance["name"], timeout=.5)
-                warnings += spf_record["warnings"]
+                for warning in spf_record["warnings"]:
+                    warnings.append("SPF: {}".format(warning))
             except checkdmarc.SPFError as e:
+                instance["spf_valid"] = False
                 errors.append("SPF: {}".format(e))
             instance["dmarc_policy"] = dmarc_policy
             instance["errors"] = errors
@@ -136,7 +150,7 @@ def _main():
         csv_file.seek(0)
         output = csv_file.read()
     if args.output:
-        with open(args.output, "w", newline="\n", encoding="utf-16", errors="replace") as output_file:
+        with open(args.output, "w", newline="\n", encoding="utf-8", errors="replace") as output_file:
             output_file.write(output)
     else:
         print(output)
